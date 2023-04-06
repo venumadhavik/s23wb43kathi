d2948b522a7ba920609a6542686b345d5f131a2a
venumadhavik <123043484+venumadhavik@users.noreply.github.com>
2023-04-05 22:12:45 -0500

    use a good message

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var carRouter = require('./routes/car');
+var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
+
+
 
 var app = express();
 
@@ -21,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/car', carRouter);
+app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
